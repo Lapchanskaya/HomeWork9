33d45d4cc9b57b40bafd8051b936ed8ef4656bd3
Svetlana <isaeva322@mail.ru>
2022-12-04 01:05:42 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        int [] array = arrayRandom (30);
+
+        // Задание 1
+        //
+        //Первым делом бухгалтеры попросили посчитать сумму всех выплат за месяц.
+        //
+        //Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+        //
+        //в формате: «Сумма трат за месяц составила … рублей».
+        System.out.println( " Задача 1") ;
+
+        int sum = 0;
+        for( int i : array){
+            sum = sum + i;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей. ");
+
+
+
     }
+
+    private static int[] arrayRandom(int length) {
+        Random random = new Random();
+        int [] array = new int[length];
+        for (int i = 0; i < length; i++){
+            array[i] = random.nextInt(100_000) + 100_000;
+        }
+        return array;
+
+    }
+
+
+
+
+
 }
